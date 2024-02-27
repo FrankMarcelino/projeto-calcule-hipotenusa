5b8c8c324a23111d3670a4dfb8dc498030467060
frankMarcelino <frank.silva.ti@gmail.com>
2024-02-27 08:25:54 -0300

    trantado erros

--- calculatePythagoras.js
+++ calculatePythagoras.js
@@ -1,16 +1,20 @@
 function calculateSquareRoot(radicand) {
-  console.log('O valor do meu radicando é: ', radicand);
-  return radicand ** (1 / 2);
+  try {
+    if (radicand <= 0) throw new Error("O radicando deve ser real e positivo");
+    return radicand ** (1 / 2);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 function calculatePythagoras(side1, side2) {
   console.log(
-    'Os tamanhos dos catetos do meu triângulo retângulo são: ',
+    "Os tamanhos dos catetos do meu triângulo retângulo são: ",
     side1,
     side2
   );
   const sum = side1 ** 2 + side2 ** 2;
-  console.log('O valor da variável sum é: ', sum);
+  console.log("O valor da variável sum é: ", sum);
   return calculateSquareRoot(sum);
 }
 
@@ -18,23 +22,37 @@ function calculatePythagoras(side1, side2) {
 //   `A hipotenusa de um triangulo de lados 3 e 4 é: ${calculatePythagoras(3, 4)}`
 // );
 
-const firstSideRef = document.getElementById('side-1');
-const secondSideRef = document.getElementById('side-2');
-const calculateButton = document.getElementById('calculate-hypotenuse');
-const answerDiv = document.getElementById('answer');
+const firstSideRef = document.getElementById("side-1");
+const secondSideRef = document.getElementById("side-2");
+
+const calculateButton = document.getElementById("calculate-hypotenuse");
+const answerDiv = document.getElementById("answer");
 
 function onCalculateButtonClick() {
-  const firstSide = firstSideRef.value;
-  const secondSide = secondSideRef.value;
-  console.log('function executed');
-  answerDiv.classList.remove('hidden-element');
-  answerDiv.innerHTML = `A hipotenusa de um triangulo de lados ${firstSide} e ${secondSide} é: ${calculatePythagoras(
-    firstSide,
-    secondSide
-  )
-    .toFixed(3)
-    .replace(/[.,]000$/, '')}
-  <div class="left-point"></div>`;
+  try {
+    const firstSide = Number(firstSideRef.value);
+    const secondSide = Number(secondSideRef.value);
+    if (
+      firstSide === "" ||
+      secondSide === "" ||
+      isNaN(firstSide) ||
+      isNaN(secondSide) ||
+      firstSide <= 0 ||
+      secondSide <= 0
+    ) {
+      throw new Error("Insira apenas números positivos");
+    }
+    answerDiv.classList.remove("hidden-element");
+    answerDiv.innerHTML = `A hipotenusa de um triangulo de lados ${firstSide} e ${secondSide} é: ${calculatePythagoras(
+      firstSide,
+      secondSide
+    )
+      .toFixed(3)
+      .replace(/[.,]000$/, "")}
+    <div class="left-point"></div>`;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-calculateButton.addEventListener('click', onCalculateButtonClick);
+calculateButton.addEventListener("click", onCalculateButtonClick);
